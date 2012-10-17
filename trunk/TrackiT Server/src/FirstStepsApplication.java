7f0029f340b517171a732f26a718b4e76670ea90
jamescross91 <jamescross91@419f4772-89f9-4321-a8d5-56703c9a77e9>
2012-10-17 16:37:15 +0000

    Initial push

---
+++ trunk/TrackiT Server/src/FirstStepsApplication.java
@@ -0,0 +1,34 @@
+import org.restlet.Application;
+import org.restlet.Restlet;
+import org.restlet.routing.Router;
+
+//Another test
+
+// Rebasing 
+
+// Add another comment
+
+
+
+// Remote ch\ange
+
+//Local change
+
+
+public class FirstStepsApplication extends Application {
+
+    /**
+     * Creates a root Restlet that will receive all incoming calls.
+     */
+    @Override
+    public synchronized Restlet createInboundRoot() {
+        // Create a router Restlet that routes each call to a new instance of HelloWorldResource.
+        Router router = new Router(getContext());
+
+        // Defines only one route
+        router.attach("/hello", HelloWorldResource.class);
+
+        return router;
+    }
+
+}
